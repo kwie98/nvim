f93291cd1042201b915dc2ac540fdd831784bb79
Konrad Wienecke <konrad.wienecke@mac.com>
2022-11-01 10:28:38 +0100

    Name TS parsers explicitly

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,95 +1,125 @@
-U.load({ "nvim-treesitter.configs", "treesitter-context", "tsht" }, function(configs, context)
-    -- nvim-treehopper
-    -- vim.keymap.set("o", "m", tsht.nodes, {noremap = true, silent = true, desc = "Treehopper"})
-    -- vim.keymap.set("x", "m", tsht.nodes, {noremap = true, silent = true, desc = "Treehopper"})
-    vim.cmd([[
-        omap     <silent> m :<C-U>lua require('tsht').nodes()<CR>
-        xnoremap <silent> m :lua require('tsht').nodes()<CR>
-    ]])
+U.load(
+    { "nvim-treesitter.configs", "nvim-treesitter.parsers", "treesitter-context" },
+    function(configs, parsers, context)
+        -- nvim-treehopper
+        -- vim.keymap.set("o", "m", tsht.nodes, {noremap = true, silent = true, desc = "Treehopper"})
+        -- vim.keymap.set("x", "m", tsht.nodes, {noremap = true, silent = true, desc = "Treehopper"})
+        vim.cmd([[
+            omap     <silent> m :<C-U>lua require('tsht').nodes()<CR>
+            xnoremap <silent> m :lua require('tsht').nodes()<CR>
+        ]])
 
-    configs.setup({
-        playground = {
-            enable = true,
-        },
-        auto_install = true,
-        highlight = {
-            enable = true, -- false will disable the whole extension
-            disable = { "css" }, -- list of language that will be disabled
-        },
-        autopairs = {
-            enable = true,
-        },
-        indent = { enable = true, disable = { "css", "python", "yaml" } },
-        incremental_selection = {
-            enable = true,
-            keymaps = {
-                -- init_selection = "gnn",
-                -- node_incremental = ",",
-                -- scope_incremental = "grc",
-                -- node_decremental = "–",
+        local parser_configs = parsers.get_parser_configs()
+        parser_configs.haskell = {
+            install_info = {
+                url = "https://github.com/tree-sitter/tree-sitter-haskell",
+                files = { "src/parser.c", "src/scanner.c" },
             },
-        },
-        matchup = {
-            enable = true,
-            disable_virtual_text = true,
-        },
-        textobjects = {
-            select = {
+        }
+
+        configs.setup({
+            ensure_installed = {
+                "bash",
+                -- "bibtex",
+                -- "cmake",
+                "comment",
+                "diff",
+                "fish",
+                "gitignore",
+                "haskell",
+                "help",
+                "json",
+                "latex",
+                "lua",
+                "markdown",
+                "python",
+                "toml",
+                "vim",
+                "yaml",
+            },
+            playground = {
                 enable = true,
-                lookahead = true,
+            },
+            auto_install = true,
+            highlight = {
+                enable = true, -- false will disable the whole extension
+                disable = { "css" }, -- list of language that will be disabled
+            },
+            autopairs = {
+                enable = true,
+            },
+            indent = { enable = true, disable = { "css", "python", "yaml" } },
+            incremental_selection = {
+                enable = true,
                 keymaps = {
-                    -- ["ab"] = "@block.outer", -- meh
-                    -- ["ib"] = "@block.inner",
-                    ["af"] = "@call.outer",
-                    ["if"] = "@call.inner",
-                    ["ac"] = "@comment.outer", -- only does one line
-                    ["ic"] = "@comment.outer",
-                    -- ["ai"] = "@conditional.outer", -- eh
-                    -- ["ii"] = "@conditional.inner",
-                    ["am"] = "@function.outer", -- actually goated
-                    ["im"] = "@function.inner",
-                    -- ["al"] = "@loop.outer", -- meh
-                    -- ["il"] = "@loop.inner",
-                    ["aa"] = "@parameter.outer",
-                    ["ia"] = "@parameter.inner",
-                    -- ["aS"] = "@statement.outer", -- meh
-                    -- ["iS"] = "@statement.outer",
+                    -- init_selection = "gnn",
+                    -- node_incremental = ",",
+                    -- scope_incremental = "grc",
+                    -- node_decremental = "–",
                 },
             },
-            move = {
+            matchup = {
                 enable = true,
-                goto_next_start = {
-                    [",w"] = "@parameter.inner",
-                    ["]a"] = "@parameter.inner",
-                    ["]]"] = "@class.outer",
-                    ["]m"] = "@function.outer",
-                    ["]f"] = "@call.outer",
-                    ["]c"] = "@comment.outer",
+                disable_virtual_text = true,
+            },
+            textobjects = {
+                select = {
+                    enable = true,
+                    lookahead = true,
+                    keymaps = {
+                        -- ["ab"] = "@block.outer", -- meh
+                        -- ["ib"] = "@block.inner",
+                        ["af"] = "@call.outer",
+                        ["if"] = "@call.inner",
+                        ["ac"] = "@comment.outer", -- only does one line
+                        ["ic"] = "@comment.outer",
+                        -- ["ai"] = "@conditional.outer", -- eh
+                        -- ["ii"] = "@conditional.inner",
+                        ["am"] = "@function.outer", -- actually goated
+                        ["im"] = "@function.inner",
+                        -- ["al"] = "@loop.outer", -- meh
+                        -- ["il"] = "@loop.inner",
+                        ["aa"] = "@parameter.outer",
+                        ["ia"] = "@parameter.inner",
+                        -- ["aS"] = "@statement.outer", -- meh
+                        -- ["iS"] = "@statement.outer",
+                    },
                 },
-                goto_next_end = {
-                    [",e"] = "@parameter.inner",
-                    ["]A"] = "@parameter.inner",
-                    ["]M"] = "@function.outer",
-                    ["]F"] = "@call.outer",
-                    ["]C"] = "@comment.outer",
-                },
-                goto_previous_start = {
-                    [",b"] = "@parameter.inner",
-                    ["[a"] = "@parameter.inner",
-                    ["[["] = "@class.outer",
-                    ["[m"] = "@function.outer",
-                    ["[f"] = "@call.outer",
-                    ["[c"] = "@comment.outer",
-                },
-                goto_previous_end = {
-                    [",s"] = "@parameter.inner",
-                    ["[A"] = "@parameter.inner",
-                    ["[M"] = "@function.outer",
-                    ["[F"] = "@call.outer",
-                    ["[C"] = "@comment.outer",
+                move = {
+                    enable = true,
+                    goto_next_start = {
+                        [",w"] = "@parameter.inner",
+                        ["]a"] = "@parameter.inner",
+                        ["]]"] = "@class.outer",
+                        ["]m"] = "@function.outer",
+                        ["]f"] = "@call.outer",
+                        ["]c"] = "@comment.outer",
+                    },
+                    goto_next_end = {
+                        [",e"] = "@parameter.inner",
+                        ["]A"] = "@parameter.inner",
+                        ["]M"] = "@function.outer",
+                        ["]F"] = "@call.outer",
+                        ["]C"] = "@comment.outer",
+                    },
+                    goto_previous_start = {
+                        [",b"] = "@parameter.inner",
+                        ["[a"] = "@parameter.inner",
+                        ["[["] = "@class.outer",
+                        ["[m"] = "@function.outer",
+                        ["[f"] = "@call.outer",
+                        ["[c"] = "@comment.outer",
+                    },
+                    goto_previous_end = {
+                        [",s"] = "@parameter.inner",
+                        ["[A"] = "@parameter.inner",
+                        ["[M"] = "@function.outer",
+                        ["[F"] = "@call.outer",
+                        ["[C"] = "@comment.outer",
+                    },
                 },
             },
-        },
-    })
-    context.setup()
-end)
+        })
+        context.setup()
+    end
+)
