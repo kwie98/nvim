d6c199913d551030b77814e12058d145b84b9084
Konrad Wienecke <konrad.wienecke@mac.com>
2023-02-26 02:42:57 +0100

    Add some default which-key helps

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -20,9 +20,9 @@ return {
                         operators = false, -- adds help for operators like d, y, ... and registers them for motion / text object completion
                         motions = false, -- adds help for motions
                         text_objects = false, -- help for text objects triggered after entering an operator
-                        windows = false, -- default bindings on <c-w>
-                        nav = false, -- misc bindings to work with windows
-                        z = false, -- bindings for folds, spelling and others prefixed with z
+                        windows = true, -- default bindings on <c-w>
+                        nav = true, -- misc bindings to work with windows
+                        z = true, -- bindings for folds, spelling and others prefixed with z
                         g = true, -- bindings for prefixed with g
                     },
                 },
