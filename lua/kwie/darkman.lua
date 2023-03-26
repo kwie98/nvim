888ac54a1f5e0c64daaab5e6ed92afb8b52731ab
Konrad Wienecke <konrad.wienecke@mac.com>
2023-03-26 11:39:13 +0200

    Use darkman only if available on system

--- lua/kwie/darkman.lua
+++ lua/kwie/darkman.lua
@@ -1,13 +1,15 @@
 -- query system color scheme (~8ms)
 local background = vim.trim(vim.fn.system("darkman get"))
-vim.opt.background = background
-local colorscheme
-if background == "light" then
-    colorscheme = "tokyonight-day"
-else
-    -- moon is a bit darker and has some different hl groups and colors, night is quite dark and contrasty
-    colorscheme = "tokyonight-night"
-end
+local colorscheme = "tokyonight-night" -- default
+if background == "dark" or background == "light" then
+    vim.opt.background = background
+    if background == "light" then
+        colorscheme = "tokyonight-day"
+        -- moon is a bit darker and has some different hl groups and colors, night is quite dark and contrasty
+    end
 
-pcall(vim.cmd.colorscheme, colorscheme)
-pcall(vim.cmd.colorscheme, colorscheme) -- helps with bufferline highlights
+    pcall(vim.cmd.colorscheme, colorscheme)
+    pcall(vim.cmd.colorscheme, colorscheme) -- helps with bufferline highlights
+else
+    pcall(vim.cmd.colorscheme, colorscheme)
+end
