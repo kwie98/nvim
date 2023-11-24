ce8f2c10f8e5e74008f44c72b0194aa0b54a976a
Konrad Wienecke <konrad.wienecke@mac.com>
2023-11-24 20:57:21 +0100

    Move `project.nvim` config

--- lua/plugins/telescope/init.lua
+++ lua/plugins/telescope/init.lua
@@ -74,6 +74,7 @@ return {
                             ["<C-o>"] = actions.cycle_history_prev,
 
                             ["<C-Space>"] = actions.cycle_previewers_next,
+                            ["<C-Enter>"] = actions.toggle_selection + actions.move_selection_worse,
 
                             ["<Esc>"] = actions.close,
                         },
@@ -104,32 +105,6 @@ return {
                 },
             })
             telescope.load_extension("fzf")
-        end,
-    },
-    {
-        "ahmedkhalf/project.nvim",
-        dependencies = {
-            "nvim-telescope/telescope.nvim",
-        },
-        event = "VeryLazy",
-
-        config = function()
-            local project = require("project_nvim")
-            local telescope = require("telescope")
-
-            project.setup({
-                manual_mode = true,
-                detection_methods = { "pattern" },
-                -- patterns = { ">Python", ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json" },
-                patterns = { ".git" }, -- ">"-patterns break fugitive sadly
-                ignore_lsp = {},
-                show_hidden = true,
-                silent_chdir = true,
-                datapath = vim.fn.stdpath("data"),
-            })
-
-            vim.keymap.set("n", "<Leader>j", telescope.extensions.projects.projects, { desc = "Projects" })
-            vim.keymap.set("n", "<Leader>J", "<CMD>ProjectRoot<CR>", {desc = "Project CWD"})
         end,
     },
 }
