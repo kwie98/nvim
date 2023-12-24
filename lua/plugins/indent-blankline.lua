0a5c7465bbcab44448b572ada1e3b9c9e38d9e32
Konrad Wienecke <konrad.wienecke@gmail.com>
2023-12-24 12:55:02 +0100

    Add unused indent-blankline conf

---
+++ lua/plugins/indent-blankline.lua
@@ -0,0 +1,17 @@
+return {
+    -- "lukas-reineke/indent-blankline.nvim",
+    -- event = "VeryLazy",
+
+    -- config = function()
+    --     require("ibl").setup({
+    --         scope = {
+    --             enabled = false,
+    --             show_start = false,
+    --             show_end = false,
+    --         },
+    --         indent = {
+    --             char = "▏",
+    --         },
+    --     })
+    -- end,
+}
