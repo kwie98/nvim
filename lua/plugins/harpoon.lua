03a4247073544c5ee2b0118dbe838ac756642237
Konrad Wienecke <konrad.wienecke@gmail.com>
2023-12-24 12:58:47 +0100

    Attempt at doing terminals with harpoon

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -3,6 +3,7 @@ return {
     branch = "harpoon2",
     dependencies = { "nvim-lua/plenary.nvim" },
     keys = {
+        -- { "<Leader>H", mode = "n", desc = "Harpoon Terminals" },
         { "<Leader>a", mode = "n", desc = "Add to Harpoon" },
         { "<Leader>h", mode = "n", desc = "Harpoon" },
         { "<Leader>u", mode = "n", desc = "Harpoon First" },
@@ -13,6 +14,8 @@ return {
 
     config = function()
         local harpoon = require("harpoon")
+        -- local Terminal = require("toggleterm.terminal").Terminal
+        -- local terminals = {}
 
         harpoon:setup({
             settings = {
@@ -20,7 +23,42 @@ return {
                 save_on_toggle = true,
                 sync_on_ui_close = true,
             },
+            -- terminals = {
+            --     select = function(list_item, list, _)
+            --         local root = list.config.get_root_dir()
+            --         local cmd = list_item.value
+            --         -- local terminal = terminals[root][cmd]
+            --         if terminals[root] == nil then
+            --             terminals[root] = {}
+            --         end
+            --         if terminals[root] ~= nil and terminals[root][cmd] ~= nil then
+            --             terminals[root][cmd]:toggle()
+            --             return
+            --         end
+            --         -- terminals[root] =
+            --         local terminal = Terminal:new({
+            --             cmd = cmd,
+            --             direction = "float",
+            --             hidden = true,
+            --         })
+            --         terminals[root][cmd] = terminal
+
+            --         terminal:toggle()
+            --     end,
+            -- },
         })
+
+        -- local terminals = require("harpoon.list").decode({
+        --     terminals = {
+        --         select = function(list_item, _, _) print(list_item.value) end,
+        --     },
+        -- }, "terminals", {})
+        -- local terminals = require("harpoon.list"):new({
+        --     select = function(list_item, _, _) print(list_item.value) end,
+        -- })
+        -- vim.keymap.set("n", "<Leader>H", function() harpoon.ui:toggle_quick_menu(harpoon:list("terminals")) end)
+        -- require("harpoon.data")
+        -- harpoon.lists[terminals]
 
         vim.keymap.set("n", "<Leader>a", function() harpoon:list():append() end)
         vim.keymap.set("n", "<Leader>h", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
