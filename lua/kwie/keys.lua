24f3f860ebe39ab6dcef0c158acfc642050dd2c1
Konrad Wienecke <konrad.wienecke@gmail.com>
2023-12-24 12:41:49 +0100

    Updated keybinds

--- lua/kwie/keys.lua
+++ lua/kwie/keys.lua
@@ -16,30 +16,39 @@ vim.keymap.set("n", "k", function()
     return "k"
 end, { expr = true })
 
--- Fast file saving and quitting:
-vim.keymap.set("n", "<Leader>w", "<CMD>w<Enter>", { desc = "Save" })
-vim.keymap.set("n", "<Leader>q", "<CMD>q<Enter>", { desc = "Quit" })
-
 -- Wiki:
 local wiki_index = vim.fn.expand("~/Sync/wiki/README.md")
 vim.keymap.set("n", "<Leader>W", function() vim.cmd.edit(wiki_index) end, { desc = "Wiki" })
 
+-- Quickfix:
 vim.keymap.set("n", "<Leader>C", "<CMD>copen<Enter>", { desc = "Quickfix" })
 vim.keymap.set("n", "]c", "<CMD>cnext<Enter>")
 vim.keymap.set("n", "[c", "<CMD>cprev<Enter>")
 
--- Navigation:
+-- Windows:
 vim.keymap.set("n", "<C-j>", "<C-w>w")
 vim.keymap.set("n", "<C-k>", "<C-w>W")
-vim.keymap.set("n", "<C-h>", "<CMD>tabprevious<Enter>")
-vim.keymap.set("n", "<C-l>", "<CMD>tabnext<Enter>")
-vim.keymap.set("n", "<C-w>t", "<CMD>tabnew %<Enter>")
 
--- Move windows:
 vim.keymap.set("n", "<C-A-h>", "<C-w>H")
 vim.keymap.set("n", "<C-A-j>", "<C-w>J")
 vim.keymap.set("n", "<C-A-k>", "<C-w>K")
 vim.keymap.set("n", "<C-A-l>", "<C-w>L")
+
+-- vim.keymap.set("n", "<Leader>x", "<C-w>s")
+-- vim.keymap.set("n", "<Leader>v", "<C-w>v")
+vim.keymap.set("n", "<Leader>q", "<CMD>q<Enter>", { desc = "Quit" })
+
+-- Fast file saving and quitting:
+vim.keymap.set("n", "<Leader>w", "<CMD>w<Enter>", { desc = "Save" })
+
+-- Tabs:
+vim.keymap.set("n", "<C-h>", "<CMD>tabprevious<Enter>")
+vim.keymap.set("n", "<C-l>", "<CMD>tabnext<Enter>")
+vim.keymap.set("n", "<C-w>t", "<CMD>tabnew %<Enter>")
+-- vim.keymap.set("n", "<C-h>", "<C-w>h")
+-- vim.keymap.set("n", "<C-j>", "<C-w>j")
+-- vim.keymap.set("n", "<C-k>", "<C-w>k")
+-- vim.keymap.set("n", "<C-l>", "<C-w>l")
 
 -- Move text:
 vim.keymap.set("n", "<A-j>", "<Esc>:m .+1<CR>==")
@@ -73,7 +82,7 @@ vim.keymap.set("v", "<", "<gv")
 vim.keymap.set("v", ">", ">gv")
 vim.keymap.set("n", "<", "<<")
 vim.keymap.set("n", ">", ">>")
-vim.keymap.set("v", "p", '"_dP') -- keep paste buffer content when overwriting something in visual mode
+-- vim.cmd('xnoremap <expr> <C-Enter> mode() ==# "v" ? "V" : "v"')  -- toggle visual/visual-line
 
 -- One-handed copy paste:
 vim.keymap.set({ "i", "c" }, "<C-v>", "<C-r><C-o>+")
@@ -87,7 +96,7 @@ vim.keymap.set("n", "]l", vim.diagnostic.goto_next)
 vim.keymap.set("n", "[l", vim.diagnostic.goto_prev)
 
 -- Toggle diagnostic highlighting:
-vim.g.diagnostic_visible = false
+vim.g.diagnostic_visible = true
 vim.keymap.set("n", "<Leader>lh", function()
     if vim.g.diagnostic_visible then
         vim.diagnostic.config({ virtual_text = false, underline = false })
