d4658fde9335b5313fe9bcec736f8432c1fd37da
Konrad Wienecke <konrad.wienecke@gmail.com>
2023-12-24 12:46:52 +0100

    Attempt at including import organizing in formatting requests

--- lua/plugins/lsp/handlers.lua
+++ lua/plugins/lsp/handlers.lua
@@ -10,18 +10,27 @@ M.on_attach = function(_, _) -- client, bufnr
     -- client.server_capabilities.semanticTokensProvider = nil
     -- vim.opt.formatexpr = ""
 
-    -- stylua: ignore start
+    local function format_and_organize_imports()
+        vim.lsp.buf.format()
+        -- vim.lsp.buf.code_action({
+        --     apply = true,
+        --     context = {
+        --         only = { "source.organizeImports" },
+        --         diagnostics = {},
+        --     },
+        -- })
+    end
+
     vim.keymap.set("n", "<Leader>la", vim.lsp.buf.code_action, { buffer = true, desc = "Code Action" })
-    vim.keymap.set("n", "<Leader>lf", vim.lsp.buf.format,      { buffer = true, desc = "Format" })
-    vim.keymap.set("n", "<Leader>ll", vim.lsp.codelens.run,    { buffer = true, desc = "CodeLens Action" })
-    vim.keymap.set("n", "gD", vim.lsp.buf.declaration,         { buffer = true, desc = "Goto Declaration" })
-    vim.keymap.set("n", "gd", telescope.lsp_definitions,       { buffer = true, desc = "Goto Definition" })
-    vim.keymap.set("n", "gI", vim.lsp.buf.implementation,      { buffer = true, desc = "Goto Implementation" })
-    vim.keymap.set("n", "gr", telescope.lsp_references,        { buffer = true, desc = "Goto References" })
-    vim.keymap.set("n", "gK", vim.lsp.buf.signature_help,      { buffer = true, desc = "Signature Help" })
-    vim.keymap.set("i", "<C-k>", vim.lsp.buf.signature_help,   { buffer = true, desc = "Signature Help" })
-    vim.keymap.set("n", "K", vim.lsp.buf.hover,                { buffer = true, desc = "LSP Hover" })
-    -- stylua: ignore end
+    vim.keymap.set("n", "<Leader>lf", format_and_organize_imports, { buffer = true, desc = "Format" })
+    vim.keymap.set("n", "<Leader>ll", vim.lsp.codelens.run, { buffer = true, desc = "CodeLens Action" })
+    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { buffer = true, desc = "Goto Declaration" })
+    vim.keymap.set("n", "gd", telescope.lsp_definitions, { buffer = true, desc = "Goto Definition" })
+    vim.keymap.set("n", "gI", vim.lsp.buf.implementation, { buffer = true, desc = "Goto Implementation" })
+    vim.keymap.set("n", "gr", telescope.lsp_references, { buffer = true, desc = "Goto References" })
+    vim.keymap.set("n", "gK", vim.lsp.buf.signature_help, { buffer = true, desc = "Signature Help" })
+    vim.keymap.set("i", "<C-k>", vim.lsp.buf.signature_help, { buffer = true, desc = "Signature Help" })
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = true, desc = "LSP Hover" })
 
     local filter_f_symbols = { symbols = { "function", "method", "class" } }
     vim.keymap.set(
