2b4cab7e0ed27f9445407a2dd0cd15c30d52a3b4
Konrad Wienecke <konrad.wienecke@gmail.com>
2024-01-04 00:01:23 +0100

    Set colorscheme even if darkman is not available

--- lua/kwie/darkman.lua
+++ lua/kwie/darkman.lua
@@ -3,12 +3,10 @@
 -- Query system color scheme (~8ms):
 local background = vim.trim(vim.fn.system("darkman get"))
 
-if background == "dark" then
-    vim.opt.background = background
-    pcall(vim.cmd.colorscheme, "tokyonight-night")
-elseif background == "light" then
-    vim.opt.background = background
+if background == "light" then
+    vim.opt.background = "light"
     pcall(vim.cmd.colorscheme, "tokyonight-day")
-else
-    print("Got unexpected darkman answer: " .. background)
+else -- could be "dark" or something else (if darkman fails)
+    vim.opt.background = "dark"
+    pcall(vim.cmd.colorscheme, "tokyonight-night")
 end
