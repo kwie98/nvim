f532ff31f28e0833db9a0989b256d01dcaea0365
Konrad Wienecke <konrad.wienecke@gmail.com>
2024-01-03 23:50:18 +0100

    Use make for telescope-fzf-native

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,7 +6,7 @@ return {
         "aaronhallaert/advanced-git-search.nvim",
         {
             "nvim-telescope/telescope-fzf-native.nvim",
-            build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+            build = "make",
         },
     },
     event = "VeryLazy",
