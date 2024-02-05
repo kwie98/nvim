2cade9ce57eb458b02d0a2ff1e46234eddf00e0d
Konrad Wienecke <konrad.wienecke@gmail.com>
2024-02-05 13:06:06 +0100

    Use django profile for djlint in htmldjango files

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -4,11 +4,20 @@ return {
 
     config = function()
         local lint = require("lint")
+        local djlint_django = lint.linters.djlint
+        djlint_django.args = {
+            "--profile",
+            "django",
+            "--linter-output-format",
+            "{line}:{code}: {message}",
+            "-",
+        }
+        lint.linters.djlint_django = djlint_django
 
         lint.linters_by_ft = {
             bash = { "shellcheck" },
             dockerfile = { "hadolint" },
-            htmldjango = { "djlint" },
+            htmldjango = { "djlint_django" },
             lua = { "selene" },
             markdown = { "markdownlint" },
             zsh = { "zsh" },
