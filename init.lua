325cdf1f423de8e421df0f3f4126484c6156be22
Konrad Wienecke <wienecke.konrad@mh-hannover.de>
2024-04-18 14:55:14 +0200

    Add some python abbreviations

--- init.lua
+++ init.lua
@@ -1,3 +1,7 @@
+vim.cmd.abbreviate("nda", "NDArray[Any]")
+vim.cmd.abbreviate("ndb", "NDArray[np.bool_]")
+vim.cmd.abbreviate("ndf", "NDArray[np.float32]")
+
 if not vim.g.vscode then
     vim.loader.enable()
     require("kwie.options")
