e737611709f3bbb5e2b3dccda750a640d97f4b8c
Konrad Wienecke <wienecke.konrad@mh-hannover.de>
2024-06-27 14:45:31 +0200

    Fix python dap conf

--- lua/plugins/dap/configs/python.lua
+++ lua/plugins/dap/configs/python.lua
@@ -1,4 +1,15 @@
 return function(dap)
+    local function get_python_path()
+        local venv = os.getenv("VIRTUAL_ENV")
+        if venv == nil then return "/usr/bin/python" end
+        local python_path = venv .. "/bin/python"
+        if vim.fn.executable(python_path) == 1 then
+            return python_path
+        else
+            return "/usr/bin/python"
+        end
+    end
+
     dap.adapters.python = {
         type = "executable",
         command = vim.fn.stdpath("data") .. "/mason/packages/debugpy/venv/bin/python",
@@ -6,23 +17,23 @@ return function(dap)
     }
     dap.configurations.python = {
         {
+            name = "debug this file",
             type = "python",
             request = "launch",
-            name = "Debug File",
             program = "${file}",
             console = "integratedTerminal",
-            pythonPath = function()
-                local venv = os.getenv("VIRTUAL_ENV")
-                if venv == nil then
-                    return "/usr/bin/python"
-                end
-                local python_path = venv .. "/bin/python"
-                if vim.fn.executable(python_path) == 1 then
-                    return python_path
-                else
-                    return "/usr/bin/python"
-                end
-            end,
+            pythonPath = get_python_path,
+        },
+        {
+            name = "debug these tests",
+            type = "python",
+            request = "launch",
+            module = "pytest",
+            args = {
+                "${file}",
+            },
+            console = "integratedTerminal",
+            pythonPath = get_python_path,
         },
     }
 end
