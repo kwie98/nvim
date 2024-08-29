23ba6b34c6abf741ac922585ed238853f1a4c56b
Konrad Wienecke <konrad.wienecke@gmail.com>
2024-08-29 15:01:37 +0200

    Nicer oil binds

--- lua/plugins/oil-explorer.lua
+++ lua/plugins/oil-explorer.lua
@@ -4,9 +4,11 @@ return {
     keys = { { "-", mode = "n", desc = "Open Explorer" } },
 
     config = function()
-        vim.keymap.set("n", "-", require("oil").open, { desc = "Open Explorer" })
+        local oil = require("oil")
+        vim.keymap.set("n", "-", oil.open, { desc = "Open Explorer (parent)" })
+        vim.keymap.set("n", "—", function() oil.open(vim.fn.getcwd()) end, { desc = "Open Explorer (cwd)" })
 
-        require("oil").setup({
+        oil.setup({
             columns = { "permissions", "mtime", "icon" },
             delete_to_trash = true,
             cleanup_delay_ms = 0,
@@ -18,16 +20,18 @@ return {
                 ["<C-t>"] = "actions.select_tab",
                 ["<Tab>"] = "actions.preview",
                 ["-"] = "actions.parent",
-                ["<Leader>j"] = "actions.open_cwd",
+                ["J"] = { "actions.cd", opts = { scope = "tab" } },
                 ["gs"] = "actions.change_sort",
                 ["gx"] = "actions.open_external",
                 ["g."] = "actions.toggle_hidden",
                 ["g\\"] = "actions.toggle_trash",
+                ["yp"] = "actions.copy_entry_path",
             },
             view_options = {
                 show_hidden = true,
                 is_always_hidden = function(name, _) return name == ".." end,
             },
+            watch_for_changes = true,
         })
     end,
 }
