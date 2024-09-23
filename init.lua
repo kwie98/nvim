4ba803d614e6e0004d6e4e70de6b5fe687d11264
Konrad Wienecke <konrad.wienecke@gmail.com>
2024-09-23 21:24:57 +0200

    Remove abbr

--- init.lua
+++ init.lua
@@ -1,9 +1,5 @@
 vim.loader.enable()
 
-vim.cmd.abbreviate("nda", "NDArray[Any]")
-vim.cmd.abbreviate("ndb", "NDArray[np.bool_]")
-vim.cmd.abbreviate("ndf", "NDArray[np.float32]")
-
 require("kwie.options")
 require("kwie.keys")
 
