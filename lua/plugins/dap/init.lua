84e673ef16bd69462128b0ef173e514e6e474d0d
Konrad Wienecke <konrad.wienecke@gmail.com>
2025-02-20 10:35:47 +0100

    insanely epic fastapi debug

--- lua/plugins/dap/init.lua
+++ lua/plugins/dap/init.lua
@@ -134,7 +134,10 @@ return {
             table.insert(dap.configurations.python, python_config(file .. " [args ...]", file, args_input))
         end
         for file in vim.fn.system([[rg -l 'FastAPI\(.*\)']]):gmatch("[^\r\n]+") do
-            table.insert(dap.configurations.python, python_config("fastapi run " .. file, "${env:VIRTUAL_ENV}/bin/fastapi", {"run", file}))
+            table.insert(
+                dap.configurations.python,
+                python_config("fastapi run " .. file, "${env:VIRTUAL_ENV}/bin/fastapi", { "run", file })
+            )
         end
 
         -- Set up UI:
