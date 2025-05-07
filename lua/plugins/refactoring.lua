da73fd0d48126397e608be1a52a3cebd0296fe6a
Konrad Wienecke <konrad.wienecke@gmail.com>
2025-05-07 13:42:48 +0200

    fix refactor bind weirdness

--- lua/plugins/refactoring.lua
+++ lua/plugins/refactoring.lua
@@ -13,7 +13,7 @@ return {
         vim.keymap.set("x", "<Leader>le", ":Refactor extract ", { desc = "Extract to Function" })
         vim.keymap.set("x", "<Leader>lE", ":Refactor extract_to_file ", { desc = "Extract to Function in new File" })
         vim.keymap.set("x", "<Leader>lv", ":Refactor extract_var ", { desc = "Extract Expression to Variable" })
-        vim.keymap.set({ "n", "x" }, "<Leader>li", ":Refactor inline_var", { desc = "Inline Variable" })
+        vim.keymap.set({ "n", "x" }, "<Leader>li", "<CMD>Refactor inline_var<Enter>", { desc = "Inline Variable" })
         vim.keymap.set("n", "<Leader>lI", ":Refactor inline_func", { desc = "Inline Function" })
         -- vim.keymap.set("n", "<Leader>lb", ":Refactor extract_block", { desc = "Extract Block?" })
         -- vim.keymap.set("n", "<Leader>lB", ":Refactor extract_block_to_file", { desc = "Extract Block to new File?" })
