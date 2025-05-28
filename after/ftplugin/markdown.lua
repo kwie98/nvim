7a9c4d7df6dbb4454a5207a523f910a6883e03d1
Konrad Wienecke <konrad.wienecke@gmail.com>
2025-05-28 09:03:55 +0200

    change binds

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,3 +1,3 @@
 vim.opt_local.wrap = true
 vim.opt_local.breakindentopt = "shift:2"
-vim.keymap.set("n", "<Leader>rr", "<CMD>MarkdownPreview<Enter>", { buffer = true, silent = true, desc = "Preview" })
+vim.keymap.set("n", "<Leader>x", "<CMD>MarkdownPreview<Enter>", { buffer = true, silent = true, desc = "Preview" })
