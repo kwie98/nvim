12fba54e167c724c16e9005d739689642d6c0134
Konrad Wienecke <konrad.wienecke@gmail.com>
2025-06-25 17:34:15 +0200

    avante stub

---
+++ lua/plugins/avante-ai.lua
@@ -0,0 +1,14 @@
+return {
+    -- "yetone/avante.nvim",
+    -- build = "make",
+    -- event = "VeryLazy",
+    -- ---@type avante.Config
+    -- opts = {
+    --     provider = "ollama",
+    --     providers = {
+    --         ollama = {
+    --
+    --         }
+    --     }
+    -- }
+}
