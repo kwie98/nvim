f39f92f9dbe159a6135c33f6ca6ef10b2d796e68
Konrad Wienecke <konrad.wienecke@gmail.com>
2025-06-25 18:32:44 +0200

    disable ts highlight in huge files

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -88,6 +88,11 @@ return {
             auto_install = true,
             highlight = {
                 enable = true,
+                disable = function(_lang, buf)
+                    local max_filesize = 1024 * 1024 -- 1 MB
+                    local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+                    if ok and stats and stats.size > max_filesize then return true end
+                end,
             },
             indent = {
                 enable = false, -- slow in huge files
