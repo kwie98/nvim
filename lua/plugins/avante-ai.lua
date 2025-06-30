f667afc1d2b0cecb38566659c8eb352af4431abc
Konrad Wienecke <konrad.wienecke@gmail.com>
2025-07-01 00:18:43 +0200

    working (disabled) avante config

--- lua/plugins/avante-ai.lua
+++ lua/plugins/avante-ai.lua
@@ -1,5 +1,17 @@
 return {
     -- "yetone/avante.nvim",
+    -- dependencies = {
+    --     "nvim-lua/plenary.nvim",
+    --     "MunifTanjim/nui.nvim",
+    --     {
+    --         -- Make sure to set this up properly if you have lazy=true
+    --         "MeanderingProgrammer/render-markdown.nvim",
+    --         opts = {
+    --             file_types = { "markdown", "Avante" },
+    --         },
+    --         ft = { "markdown", "Avante" },
+    --     },
+    -- },
     -- build = "make",
     -- event = "VeryLazy",
     -- ---@type avante.Config
@@ -7,8 +19,15 @@ return {
     --     provider = "ollama",
     --     providers = {
     --         ollama = {
-    --
-    --         }
-    --     }
-    -- }
+    --             endpoint = "http://localhost:8080/ollama",
+    --             -- model = "llama3.1:8b",
+    --             model = "qwen2.5-coder:7b",
+    --             disable_tools = true,
+    --             -- api_key_name = "OLLAMA_API_KEY",
+    --             extra_headers = {
+    --                 ["Authorization"] = "Bearer sk-4e665db276ea4d588e3ee13658d4da9e"
+    --             }
+    --         },
+    --     },
+    -- },
 }
