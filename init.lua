e74be34e392219210229a4334ee58509e06e971c
Konrad Wienecke <konrad.wienecke@gmail.com>
2025-07-01 00:18:15 +0200

    new lsp config

--- init.lua
+++ init.lua
@@ -138,8 +138,53 @@ vim.keymap.set("n", "[l", "<CMD>cprev<Enter>")
 -- LSP:
 vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, { desc = "vim.lsp.buf.type_definition()" })
 
+-- vim.lsp.config("emmet_language_server", {
+--     filetypes = { "xml" },
+--     init_options = {
+--         -- ---@type table<string, string>
+--         -- includeLanguages = {},
+--         -- --- @type string[]
+--         -- excludeLanguages = {},
+--         -- --- @type string[]
+--         -- extensionsPath = {},
+--         -- --- @type table<string, any> [Emmet Docs](https://docs.emmet.io/customization/preferences/)
+--         -- preferences = {},
+--         --- @type boolean Defaults to `true`
+--         showAbbreviationSuggestions = true,
+--         --- @type "always" | "never" Defaults to `"always"`
+--         showExpandedAbbreviation = "always",
+--         --- @type boolean Defaults to `false`
+--         showSuggestionsAsSnippets = false,
+--         -- --- @type table<string, any> [Emmet Docs](https://docs.emmet.io/customization/syntax-profiles/)
+--         -- syntaxProfiles = {},
+--         -- --- @type table<string, string> [Emmet Docs](https://docs.emmet.io/customization/snippets/#variables)
+--         -- variables = {},
+--     },
+-- })
+vim.lsp.config("emmet_ls", {
+    filetypes = { "xml" },
+})
+vim.lsp.config("jsonls", {
+    settings = {
+        json = {
+            validate = {
+                enable = true,
+            },
+        },
+    },
+    before_init = function(_, config)
+        -- can't assign new table because of
+        -- https://github.com/neovim/neovim/issues/27740#issuecomment-1978629315
+        config.settings.json.schemas = require("schemastore").json.schemas()
+    end,
+})
+-- vim.lsp.enable("lemminx")
 vim.lsp.enable("lua_ls")
 vim.lsp.enable("basedpyright")
+vim.lsp.enable("jsonls")
+vim.lsp.enable("yamlls")
+-- vim.lsp.enable("emmet_language_server")
+vim.lsp.enable("emmet_ls")
 
 -- USER COMMANDS --
 -- Redirect command output to buffer:
