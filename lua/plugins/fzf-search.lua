339f6e2d0d4fc6126cc871dc79ce23e78b1d5cc9
Konrad Wienecke <konrad.wienecke@gmail.com>
2025-07-05 01:09:47 +0200

    more fzf binds

--- lua/plugins/fzf-search.lua
+++ lua/plugins/fzf-search.lua
@@ -5,11 +5,16 @@ return {
     -- or if using mini.icons/mini.nvim
     -- dependencies = { "echasnovski/mini.icons" },
 
-    config = function ()
+    config = function()
         local fzf = require("fzf-lua")
-        vim.keymap.set("n", "<Leader>b", fzf.buffers)
-        vim.keymap.set("n", "<Leader>f", fzf.files)
-        vim.keymap.set("n", "<Leader>t", fzf.live_grep)
+        vim.keymap.set("n", "<Leader>b", fzf.buffers, { desc = "Search Buffers" })
+        vim.keymap.set("n", "<Leader>f", fzf.files, { desc = "Search Files" })
+        vim.keymap.set("n", "<Leader>t", fzf.live_grep_native, { desc = "Live Grep" })
+        vim.keymap.set("n", "<Leader>sr", function() fzf.oldfiles({cwd="%:p:h"}) end, { desc = "Old Files" })
+        vim.keymap.set("n", "<Leader>sh", fzf.helptags, { desc = "Help" })
+        vim.keymap.set("n", "<Leader>gs", fzf.git_status, { desc = "Git Status" })
+        vim.keymap.set("n", "<Leader>sk", fzf.keymaps, {desc = "Keymaps"})
+
         fzf.setup()
-    end
+    end,
 }
