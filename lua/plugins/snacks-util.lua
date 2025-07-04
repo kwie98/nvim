6440aeaad00ab6ecc0327e8b913d2805997a4a80
Konrad Wienecke <konrad.wienecke@gmail.com>
2025-07-05 01:46:47 +0200

    yes

---
+++ lua/plugins/snacks-util.lua
@@ -0,0 +1,9 @@
+return {
+    -- "folke/snacks.nvim",
+    -- ---@type snacks.Config
+    -- opts = {
+    --     -- quickfile = {},
+    --     words = {},
+    --     -- picker = {},
+    -- },
+}
