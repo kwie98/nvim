e3137ba170a5f5a251a46e3b8068699880a00195
Konrad Wienecke <konrad.wienecke@gmail.com>
2025-07-07 17:18:11 +0200

    lsp keys

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,6 +1,10 @@
 return {
     "neovim/nvim-lspconfig",
-    dependencies = {
-        "b0o/schemastore.nvim",
+    dependencies = { "b0o/schemastore.nvim" },
+    keys = {
+        {"grI", vim.cmd.LspInfo, desc = "LSP Info"}, -- TODO
+        {"grR", vim.cmd.LspRestart, desc = "Restart LSP"},
     },
+    -- event = "VeryLazy"
+    lazy = false,
 }
