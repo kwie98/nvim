e8f33456692b82425402e95c613f371c473cecfd
Konrad Wienecke <konrad.wienecke@gmail.com>
2025-07-07 17:18:28 +0200

    epic oil keys

--- lua/plugins/oil-explorer.lua
+++ lua/plugins/oil-explorer.lua
@@ -1,45 +1,49 @@
+---@type LazySpec
 return {
     "stevearc/oil.nvim",
     -- dependencies = { "nvim-tree/nvim-web-devicons" },
-    -- keys = { { "-", mode = "n", desc = "Open Explorer" } },
+    keys = {
+        { "-", "<CMD>Oil<CR>", desc = "Oil" },
+    },
+    lazy = false,
 
-    config = function()
-        local oil = require("oil")
-        vim.keymap.set("n", "-", oil.open, { desc = "Open Explorer (parent)" })
-        vim.keymap.set("n", "—", function() oil.open(vim.fn.getcwd()) end, { desc = "Open Explorer (cwd)" })
-
-        oil.setup({
-            default_file_explorer = true,
-            -- columns = { "permissions", "mtime", "size" },
-            delete_to_trash = true,
-            cleanup_delay_ms = 0,
-            keymaps = {
-                ["g?"] = "actions.show_help",
-                ["<Enter>"] = "actions.select",
-                -- ["<C-v>"] = { "actions.select_vsplit", mode = "n" },
-                -- ["<C-s>"] = "actions.select_split",
-                -- ["<C-t>"] = "actions.select_tab",
-                ["<Tab>"] = "actions.preview",
-                ["-"] = "actions.parent",
-                ["J"] = { "actions.cd", opts = { scope = "tab" } },
-                ["gs"] = "actions.change_sort",
-                ["gx"] = "actions.open_external",
-                ["g."] = "actions.toggle_hidden",
-                ["g\\"] = "actions.toggle_trash",
-                ["<Leader>y"] = "actions.copy_entry_path",
-                ["_"] = false,
-            },
-            view_options = {
-                show_hidden = true,
-                is_always_hidden = function(name, _) return name == ".." end,
-            },
-            watch_for_changes = true,
-            preview_win = {
-                preview_method = "load", -- for dcmdump (ftplugin)
-            },
-            lsp_file_methods = {
-                timeout_ms = 5000,
-            },
-        })
-    end,
+    opts = {
+        default_file_explorer = true,
+        -- columns = { "permissions", "mtime", "size" },
+        delete_to_trash = true,
+        cleanup_delay_ms = 0,
+        keymaps = {
+            ["<F1>"] = "actions.show_help",
+            ["<Tab>"] = "actions.preview",
+            ["~"] = false,
+            ["gd"] = { "actions.cd", opts = { scope = "win" } },
+        },
+        -- keymaps = {
+        --     ["g?"] = "actions.show_help",
+        --     ["<Enter>"] = "actions.select",
+        --     -- ["<C-v>"] = { "actions.select_vsplit", mode = "n" },
+        --     -- ["<C-s>"] = "actions.select_split",
+        --     -- ["<C-t>"] = "actions.select_tab",
+        --     ["<Tab>"] = "actions.preview",
+        --     ["-"] = "actions.parent",
+        --     ["J"] = { "actions.cd", opts = { scope = "tab" } },
+        --     ["gs"] = "actions.change_sort",
+        --     ["gx"] = "actions.open_external",
+        --     ["g."] = "actions.toggle_hidden",
+        --     ["g\\"] = "actions.toggle_trash",
+        --     ["<Leader>y"] = "actions.copy_entry_path",
+        --     ["_"] = false,
+        -- },
+        view_options = {
+            show_hidden = true,
+            is_always_hidden = function(name, _) return name == ".." end,
+        },
+        watch_for_changes = true,
+        preview_win = {
+            preview_method = "load", -- for dcmdump (ftplugin)
+        },
+        lsp_file_methods = {
+            timeout_ms = 5000,
+        },
+    },
 }
