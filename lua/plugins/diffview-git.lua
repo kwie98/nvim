41a89b91b1420c555873a7f476ee5c34b2cf1391
Konrad Wienecke <konrad.wienecke@gmail.com>
2025-07-10 22:42:33 +0200

    diffview setup

--- lua/plugins/diffview-git.lua
+++ lua/plugins/diffview-git.lua
@@ -1,24 +1,49 @@
 return {
-    -- "sindrets/diffview.nvim",
+    "sindrets/diffview.nvim",
     -- lazy = true, -- loaded by telescope/advanced git search
-    --
-    -- config = function()
-    --     local diffview = require("diffview")
-    --     diffview.setup({
-    --         enhanced_diff_hl = true,
-    --         -- hooks = {
-    --         --     -- view_opened = function(view) print(vim.inspect(view)) end
-    --         --     diff_buf_read = function() vim.keymap.set("n", "<Leader>c", diffview.close, { buffer = true }) end,
-    --         -- },
-    --     })
-    --
-    --     vim.keymap.set("n", "<Leader>gs", diffview.open, { desc = "Status" })
-    --     vim.keymap.set("n", "<Leader>gh", diffview.file_history, { desc = "History" })
-    --     vim.keymap.set(
-    --         "n",
-    --         "<Leader>gf",
-    --         function() diffview.file_history(nil, { "%" }) end,
-    --         { desc = "History of this File" }
-    --     )
-    -- end,
+
+    config = function()
+        local diffview = require("diffview")
+        local actions = require("diffview.actions")
+        diffview.setup({
+            enhanced_diff_hl = true,
+            use_icons = false,
+            keymaps = {
+                disable_defaults = true,
+                view = {
+                    { "n", "<Tab>", actions.select_next_entry },
+                    { "n", "<s-Tab>", actions.select_prev_entry },
+                    { { "n", "x" }, "<CR>", ":diffget<CR>" },
+                    { "n", "<Leader><CR>", "<CMD>%diffget<CR>", { desc = "Stage whole file" } },
+                },
+                file_panel = {
+                    { "n", "<Tab>", actions.select_next_entry },
+                    { "n", "<s-Tab>", actions.select_prev_entry },
+                    { "n", "<CR>", actions.select_entry },
+                    { "n", "j", actions.next_entry },
+                    { "n", "s", actions.toggle_stage_entry },
+                    { "n", "k", actions.prev_entry },
+                },
+                file_history_panel = {
+                    { "n", "<Tab>", actions.select_next_entry },
+                    { "n", "<S-Tab>", actions.select_prev_entry },
+                    { "n", "<Leader><CR>", actions.open_in_diffview, { desc = "Open in Diffview" } },
+                    { "n", "j", actions.next_entry },
+                    { "n", "k", actions.prev_entry },
+                    { "n", "<CR>", actions.select_entry },
+                    { "n", "<C-u>", actions.scroll_view(-0.5) },
+                    { "n", "<C-d>", actions.scroll_view(0.5) },
+                },
+            },
+        })
+
+        vim.keymap.set("n", "<Leader>gd", diffview.open, { desc = "Diffview" })
+        vim.keymap.set("n", "<Leader>gh", diffview.file_history, { desc = "History" })
+        vim.keymap.set(
+            "n",
+            "<Leader>gf",
+            function() diffview.file_history(nil, { "%" }) end,
+            { desc = "History of this File" }
+        )
+    end,
 }
