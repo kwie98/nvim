95a899a29dd49b68f32a8424427a5335f8a2f959
Konrad Wienecke <konrad.wienecke@gmail.com>
2025-07-10 22:41:35 +0200

    disable neotest for now

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -1,36 +1,36 @@
 return {
-    "nvim-neotest/neotest",
-    dependencies = {
-        "nvim-neotest/nvim-nio",
-        "nvim-lua/plenary.nvim",
-        "antoinemadec/FixCursorHold.nvim",
-        "nvim-treesitter/nvim-treesitter",
-        "nvim-neotest/neotest-python",
-        "mfussenegger/nvim-dap",
-    },
-    lazy = true,
-    keys = {
-        { "<Leader>rr", mode = "n", desc = "Neotest" },
-    },
-
-    config = function()
-        local neotest = require("neotest")
-        neotest.setup({
-            adapters = {
-                require("neotest-python")({
-                    dap = { justMyCode = false },
-                    pytest_discover_instances = true,
-                }),
-            },
-            summary = {
-                mappings = {
-                    watch = "W",
-                },
-            },
-            diagnostic = {
-                enabled = false,
-            },
-        })
-        vim.keymap.set("n", "<Leader>rr", neotest.summary.open, { desc = "Neotest" })
-    end,
+    -- "nvim-neotest/neotest",
+    -- dependencies = {
+    --     "nvim-neotest/nvim-nio",
+    --     "nvim-lua/plenary.nvim",
+    --     "antoinemadec/FixCursorHold.nvim",
+    --     "nvim-treesitter/nvim-treesitter",
+    --     "nvim-neotest/neotest-python",
+    --     "mfussenegger/nvim-dap",
+    -- },
+    -- lazy = true,
+    -- keys = {
+    --     { "<Leader>rr", mode = "n", desc = "Neotest" },
+    -- },
+    --
+    -- config = function()
+    --     local neotest = require("neotest")
+    --     neotest.setup({
+    --         adapters = {
+    --             require("neotest-python")({
+    --                 dap = { justMyCode = false },
+    --                 pytest_discover_instances = true,
+    --             }),
+    --         },
+    --         summary = {
+    --             mappings = {
+    --                 watch = "W",
+    --             },
+    --         },
+    --         diagnostic = {
+    --             enabled = false,
+    --         },
+    --     })
+    --     vim.keymap.set("n", "<Leader>rr", neotest.summary.open, { desc = "Neotest" })
+    -- end,
 }
