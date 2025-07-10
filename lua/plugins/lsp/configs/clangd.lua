146027686ff282b77557b0192a343d97f5ed9d64
Konrad Wienecke <konrad.wienecke@gmail.com>
2025-07-10 23:04:34 +0200

    remove old lsp stuff

--- lua/plugins/lsp/configs/clangd.lua
+++
@@ -1,8 +0,0 @@
-return {
-    -- workaround for "warning: multiple different client offset_encodings detected for buffer, this is not supported
-    -- yet"
-    -- https://github.com/jose-elias-alvarez/null-ls.nvim/issues/428
-    capabilities = {
-        offsetEncoding = { "utf-16" },
-    },
-}
