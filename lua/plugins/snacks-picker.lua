d3b48bc9ec7ced0bc10d978440ae438db5447811
Konrad Wienecke <konrad.wienecke@gmail.com>
2025-10-25 12:35:07 +0200

    WIP git log pickaxe picker which finds commits having a search string change

--- lua/plugins/snacks-picker.lua
+++ lua/plugins/snacks-picker.lua
@@ -1,6 +1,53 @@
+local function test_picker()
+    -- local function format(item) return { { item.text, "SnacksPickerLabel" } } end
+    -- local items = {
+    --     { idx = 1, score = 1, text = "foo text", name = "foo name", file = "init.lua" },
+    --     { idx = 2, score = 2, text = "bar text", name = "bar name", file = "selene.toml" },
+    -- }
+    return require("snacks").picker({
+        live = true,
+
+        finder = function(_, ctx)
+            if ctx.filter.search == "" then return {} end
+            local result = vim.fn.system("git log --pretty=format:'%h' --name-only -S '" .. ctx.filter.search .. "'")
+            result = vim.trim(result) -- get rid of trailing newline
+            if result == "" then return {} end
+
+            ---@type snacks.picker.Item
+            local items = {}
+            for block in vim.gsplit(result, "\n\n") do
+                local lines = vim.split(block, "\n")
+                local commit = lines[1]
+                local files = { unpack(lines, 2, #lines) }
+                for _, file in ipairs(files) do
+                    table.insert(items, { commit = commit, file = file })
+                end
+            end
+            return items
+        end,
+
+        format = function(item) return { { item.file .. "\t" .. item.commit, "SnacksPickerLabel" } } end,
+        preview = function(ctx)
+            local lines = vim.fn.system("git show " .. ctx.item.commit .. ":" .. ctx.item.file)
+            ctx.preview:set_lines(vim.split(lines, "\n"))
+            local ft = vim.filetype.match({ filename = ctx.item.file })
+            if ft then ctx.preview:highlight({ ft = ft }) end
+        end,
+
+        confirm = function(picker, item)
+            picker:close()
+            -- show commit in which the search string was found in the diff, and select the file with the change:
+            require("diffview").open({
+                item.commit .. "~1.." .. item.commit,
+                "--selected-file=" .. item.file,
+            })
+        end,
+    })
+end
 ---@type LazySpec
 return {
     "folke/snacks.nvim",
+    ---@type snacks.Config
     opts = {
         picker = {
             -- your picker configuration comes here
@@ -25,5 +72,6 @@ return {
         -- { "<Leader>gt", foo, desc = "Git Grep" },
         -- { "grd", function() Snacks.picker.diagnostics_workspace() end, desc = "Workspace Diagnostics" },
         -- TODO symbols: global vars maybe???
+        { "<Leader>h", test_picker, desc = "test picker" },
     },
 }
