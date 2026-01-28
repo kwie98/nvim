4478c7596d5f9629288c8d38308416997ffe1777
Konrad Wienecke <konrad.wienecke@gmail.com>
2026-01-28 16:55:34 +0100

    open picked file in current window, even if that is an oil or terminal

--- lua/plugins/snacks-picker.lua
+++ lua/plugins/snacks-picker.lua
@@ -51,9 +51,9 @@ return {
     ---@type snacks.Config
     opts = {
         picker = {
-            -- your picker configuration comes here
-            -- or leave it empty to use the default settings
-            -- refer to the configuration section below
+            main = {
+                file = false,
+            },
         },
     },
     keys = {
